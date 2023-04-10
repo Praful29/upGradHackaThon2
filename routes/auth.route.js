284ebc6feb76da6cbf96166cf05a628e3831a5a2
Praful <95204927+Praful29@users.noreply.github.com>
2023-04-11 01:56:55 +0530

    fetch code added

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -2,4 +2,6 @@ const hotelController = require('../controllers/hotel.controller');
 
 module.exports = function (app){
     app.post('/api/restaurant/add', hotelController.addHotel);
+    app.get('/api/restaurant/', hotelController.allHotels);
+
 }
