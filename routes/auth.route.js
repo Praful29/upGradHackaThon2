e0746e783233d17856b44b6d149f00ae03ca49d9
Praful <95204927+Praful29@users.noreply.github.com>
2023-04-12 00:41:07 +0530

    category route added by praful

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -3,5 +3,7 @@ const hotelController = require('../controllers/hotel.controller');
 module.exports = function (app){
     app.post('/api/restaurant/add', hotelController.addHotel);
     app.get('/api/restaurant/', hotelController.allHotels);
+    app.get('/api/restaurant/categories', hotelController.getAllCategories);
+
 
 }
