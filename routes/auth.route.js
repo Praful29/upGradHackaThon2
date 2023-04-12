eab2f4a8cc3d918c9b711a7ef6d7b96e2a972747
Praful <95204927+Praful29@users.noreply.github.com>
2023-04-13 00:56:22 +0530

    3api added categories id rating by praful

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -4,6 +4,7 @@ module.exports = function (app){
     app.post('/api/restaurant/add', hotelController.addHotel);
     app.get('/api/restaurant/', hotelController.allHotels);
     app.get('/api/restaurant/categories', hotelController.getAllCategories);
-
-
+    app.get('/api/restaurant/categories/:categoryName', hotelController.getHotelsByCategory);
+    app.get('/api/restaurant/:id', hotelController.getHotelsById);
+    app.get('/api/restaurant/rating/:ratingValue', hotelController.getHotelsByRating);
 }
